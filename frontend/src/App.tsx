47bf3511a9a3d6f151e789cdd8b8c8d0112fa3cd
Michael Spiegel <70846025+Smightym8@users.noreply.github.com>
2025-05-21 21:37:14 +0200

    refactor: extract dependencies into component
    - Removed dependencies part from App.tsx
    - Created Dependencies.tsx component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,6 @@ import {
     Button,
     CircularProgress,
     Divider,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
     Paper,
     Typography, useColorScheme
 } from "@mui/material";
@@ -17,18 +12,16 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import * as React from "react";
 import useEdcVersions from "./hooks/useEdcVersions.ts";
-import AddDependenciesDialog from "./components/AddDependenciesDialog.tsx";
 import type {MavenPackageDTO} from "./api/models/maven-package-dto.ts";
-import RemoveCircle from '@mui/icons-material/RemoveCircle';
 import type {ProjectCreateDTO} from "./api/models/project-create-dto.ts";
 import useGenerateProject from "./hooks/useGenerateProject.ts";
 import ProjectSettings from "./components/ProjectSettings.tsx";
+import Dependencies from "./components/Dependencies.tsx";
 
 function App() {
     const {edcVersions, getEdcVersionsError, isLoading} = useEdcVersions();
     const [selectedVersion, setSelectedVersion] = React.useState<string>('');
     const [selectedVersionError, setSelectedVersionError] = React.useState<boolean>(false);
-    const [open, setOpen] = React.useState(false);
     const [selectedMavenPackages, setSelectedMavenPackages] = React.useState<MavenPackageDTO[]>([]);
     const [selectedMavenPackagesError, setSelectedMavenPackagesError] = React.useState<boolean>(false);
     const [projectName, setProjectName] = React.useState<string>('');
@@ -37,14 +30,6 @@ function App() {
     const [groupIdError, setGroupIdError] = React.useState<boolean>(false);
     const {isGeneratingProject, createProject} = useGenerateProject();
     const {mode, setMode} = useColorScheme();
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     const handleSelect = (value: MavenPackageDTO) => () => {
         const currentIndex = selectedMavenPackages.findIndex(item => item.id === value.id);
@@ -112,190 +97,116 @@ function App() {
     };
 
     return (
-        <>
-            <Box sx={{
-                height: '100%',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '2em',
-                padding: '2.5em 0',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}>
-                <Box sx={{display: 'flex', alignItems: 'center', width: '100%'}}>
-                    <Typography
-                        component="h1"
-                        variant="h3"
-                        sx={{
-                            position: 'absolute',
-                            left: '50%',
-                            transform: 'translateX(-50%)',
-                        }}
-                    >
-                        EDC Project Initializer
-                    </Typography>
-                    <Box sx={{marginLeft: 'auto', zIndex: 1, pr: 10}}>
-                        {mode === 'dark' ? (
-                            <LightModeIcon
-                                sx={{cursor: 'pointer', fontSize: 40}}
-                                onClick={() => setMode('light')}
-                            />
-                        ) : (
-                            <DarkModeIcon
-                                sx={{cursor: 'pointer', fontSize: 40}}
-                                onClick={() => setMode('dark')}
-                            />
-                        )}
-                    </Box>
+        <Box sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '2em',
+            padding: '2.5em 0',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Box sx={{display: 'flex', alignItems: 'center', width: '100%'}}>
+                <Typography
+                    component="h1"
+                    variant="h3"
+                    sx={{
+                        position: 'absolute',
+                        left: '50%',
+                        transform: 'translateX(-50%)',
+                    }}
+                >
+                    EDC Project Initializer
+                </Typography>
+                <Box sx={{marginLeft: 'auto', zIndex: 1, pr: 10}}>
+                    {mode === 'dark' ? (
+                        <LightModeIcon
+                            sx={{cursor: 'pointer', fontSize: 40}}
+                            onClick={() => setMode('light')}
+                        />
+                    ) : (
+                        <DarkModeIcon
+                            sx={{cursor: 'pointer', fontSize: 40}}
+                            onClick={() => setMode('dark')}
+                        />
+                    )}
                 </Box>
+            </Box>
 
-                {(() => {
-                    if (isLoading) {
-                        return <CircularProgress/>;
-                    } else if (getEdcVersionsError) {
-                        return (
+            {(() => {
+                if (isLoading) {
+                    return <CircularProgress/>;
+                } else if (getEdcVersionsError) {
+                    return (
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Alert severity="error" variant="outlined" sx={{maxWidth: 500}}>
+                                {getEdcVersionsError}
+                            </Alert>
+                        </Box>
+                    );
+                } else {
+                    return (
+                        <Paper
+                            elevation={6}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                minWidth: 1200,
+                                minHeight: 600,
+                                maxHeight: 600,
+                                padding: '1em'
+                            }}
+                        >
                             <Box
                                 sx={{
-                                    flexGrow: 1,
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    flex: 1,
+                                    overflowY: 'hidden',
+                                }}
+                            >
+                                <ProjectSettings edcVersions={edcVersions} selectedVersion={selectedVersion}
+                                                 setSelectedVersion={setSelectedVersion}
+                                                 selectedVersionError={selectedVersionError}
+                                                 projectName={projectName} setProjectName={setProjectName}
+                                                 projectNameError={projectNameError} groupId={groupId}
+                                                 setGroupId={setGroupId} groupIdError={groupIdError}/>
+
+                                <Divider orientation="vertical" variant="middle" flexItem/>
+
+                                <Dependencies selectedVersion={selectedVersion}
+                                              selectedMavenPackages={selectedMavenPackages}
+                                              selectedMavenPackagesError={selectedMavenPackagesError}
+                                              handleSelect={handleSelect}/>
+                            </Box>
+                            <Box
+                                sx={{
                                     display: 'flex',
                                     justifyContent: 'center',
                                     alignItems: 'center',
+                                    marginTop: '6em'
                                 }}
                             >
-                                <Alert severity="error" variant="outlined" sx={{maxWidth: 500}}>
-                                    {getEdcVersionsError}
-                                </Alert>
+                                <Button
+                                    variant='contained'
+                                    loading={isGeneratingProject}
+                                    onClick={handleGenerateProject}>
+                                    Generate Project
+                                </Button>
                             </Box>
-                        );
-                    } else {
-                        return (
-                            <Paper
-                                elevation={6}
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    minWidth: 1200,
-                                    minHeight: 600,
-                                    maxHeight: 600,
-                                    padding: '1em'
-                                }}
-                            >
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        flexDirection: 'row',
-                                        flex: 1,
-                                        overflowY: 'hidden',
-                                    }}
-                                >
-                                    <ProjectSettings edcVersions={edcVersions} selectedVersion={selectedVersion}
-                                                     setSelectedVersion={setSelectedVersion}
-                                                     selectedVersionError={selectedVersionError}
-                                                     projectName={projectName} setProjectName={setProjectName}
-                                                     projectNameError={projectNameError} groupId={groupId}
-                                                     setGroupId={setGroupId} groupIdError={groupIdError}/>
-
-                                    <Divider orientation="vertical" variant="middle" flexItem/>
-
-                                    <Box sx={{flex: 1, padding: '1em'}}>
-                                        <Box sx={{display: 'flex', marginBottom: '1em'}}>
-                                            <Typography
-                                                component="h2"
-                                                variant="h4"
-                                            >
-                                                Dependencies
-                                            </Typography>
-
-                                            <Button
-                                                variant="contained"
-                                                sx={{marginLeft: 'auto'}}
-                                                onClick={handleClickOpen}
-                                                disabled={selectedVersion === ''}
-                                            >
-                                                Add Dependencies
-                                            </Button>
-                                        </Box>
-                                        {
-                                            selectedMavenPackages.length > 0 ? (
-                                                <Box sx={{flexGrow: 1, height: '80%', overflowY: 'auto', p: 2}}>
-                                                    <List sx={{width: '100%', bgcolor: 'background.paper'}}>
-                                                        {selectedMavenPackages.map((mavenPackage: MavenPackageDTO) => (
-                                                            <React.Fragment key={mavenPackage.id}>
-                                                                <ListItem>
-                                                                    <ListItemButton
-                                                                        onClick={handleSelect(mavenPackage)}>
-                                                                        <ListItemText
-                                                                            id={mavenPackage.id}
-                                                                            primary={mavenPackage.name}
-                                                                        />
-                                                                        <ListItemIcon>
-                                                                            <RemoveCircle color="error"/>
-                                                                        </ListItemIcon>
-                                                                    </ListItemButton>
-                                                                </ListItem>
-                                                                <Divider/>
-                                                            </React.Fragment>
-                                                        ))}
-                                                    </List>
-                                                </Box>
-                                            ) : (
-                                                <Box sx={{
-                                                    flexGrow: 1,
-                                                    display: 'flex',
-                                                    flexDirection: 'column',
-                                                    justifyContent: 'center',
-                                                    alignItems: 'center',
-                                                    height: '80%',
-                                                    color: 'text.secondary'
-                                                }}>
-                                                    <Typography sx={{
-                                                        color: 'text.secondary'
-                                                    }}>
-                                                        No dependencies selected
-                                                    </Typography>
-
-                                                    {selectedMavenPackagesError &&
-                                                        <Alert severity="error" variant="outlined"
-                                                               sx={{marginTop: '1em'}}>
-                                                            You have to select at least one dependency.
-                                                        </Alert>
-                                                    }
-                                                </Box>
-                                            )
-                                        }
-                                    </Box>
-
-                                </Box>
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        marginTop: '6em'
-                                    }}
-                                >
-                                    <Button
-                                        variant='contained'
-                                        loading={isGeneratingProject}
-                                        onClick={handleGenerateProject}>
-                                        Generate Project
-                                    </Button>
-                                </Box>
-                            </Paper>
-                        )
-                            ;
-                    }
-                })()}
-            </Box>
-            <AddDependenciesDialog
-                open={open}
-                handleClose={handleClose}
-                selectedVersion={selectedVersion}
-                selectedMavenPackages={selectedMavenPackages}
-                handleSelect={handleSelect}
-            />
-        </>
+                        </Paper>
+                    );
+                }
+            })()}
+        </Box>
     )
 }
 
